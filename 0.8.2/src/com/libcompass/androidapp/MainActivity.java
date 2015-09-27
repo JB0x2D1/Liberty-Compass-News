fea50e53ec3c7ae98a8e9bd12f0229b723b183b5
JB0x2D1 <jeremy72179@gmail.com>
2015-09-26 22:38:02 -0400

    First commit

---
+++ 0.8.2/src/com/libcompass/androidapp/MainActivity.java
@@ -0,0 +1,215 @@
+package com.libcompass.androidapp;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@SuppressLint("SetJavaScriptEnabled") 
+public class MainActivity extends Activity {
+	protected static final String version = "0.8.2";
+	/* Display www.libcompass.com in the main WebView and ads in the small
+	 * WebView. */
+	//TODO: fix null pointer in webview reload
+	private static Activity act;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		act = this;
+		PlaceholderFragment frag = new PlaceholderFragment();
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, frag).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+        if (id == R.id.menu_home) {
+        	WebView myWebView = (WebView) findViewById(R.id.webview);
+        	myWebView.loadUrl("http://www.libcompass.com");
+            return true;
+        }
+        if (id == R.id.menu_about) {
+        	new AlertDialog.Builder(this)
+            .setTitle("About")
+            .setMessage("Liberty Compass News\nVersion: " + version + "\n\nReport Bugs: android@libcompass.com")
+            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) { 
+                }
+             })
+            .setIcon(android.R.drawable.ic_dialog_info)
+             .show();
+        }
+        if (id == R.id.menu_check_update) {
+        	if (MyWidgetProvider.newVersionAvailable(getBaseContext())) {
+        		new AlertDialog.Builder(this)
+                .setTitle("Update Available")
+                .setMessage("There is a new version of Liberty Compass News available.\nCheck notifications to update.")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) { 
+                    }
+                 })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                 .show();
+        	} else {
+        		new AlertDialog.Builder(this)
+                .setTitle("No Update Available")
+                .setMessage("This version of Liberty Compass News is up to date.")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) { 
+                    }
+                 })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                 .show();
+        	}
+        }
+        return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+		
+		private static WebView wv, av;
+		private Bundle webViewBundle;
+		
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View v = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			if (webViewBundle != null) {
+				wv.restoreState(webViewBundle); 
+				av.restoreState(webViewBundle);
+			}
+			else {
+				wv = (WebView) v.findViewById(R.id.webview);
+				wv.getSettings().setJavaScriptEnabled(true);
+				wv.setWebViewClient(new WebViewClient());
+				av = (WebView) v.findViewById(R.id.adview);
+				av.getSettings().setJavaScriptEnabled(true);
+				av.setWebViewClient(new WebViewClient(){
+		    	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+		    	        if (url != null) {
+		    	            view.getContext().startActivity(
+		    	                new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+		    	            return true;
+		    	        } else {
+		    	            return false;
+		    	        }
+		    	    }
+		    	});
+				wv.loadUrl("http://www.libcompass.com/");
+				av.loadUrl("http://www.libcompass.com/ads.htm");
+			}
+			//new ReloadWebView(this, 60, ((WebView)findViewById(R.id.adview)));//refresh ads after 60 seconds
+			return v;
+		}
+		
+		@Override
+	    public void onSaveInstanceState(Bundle outState) {
+	        super.onSaveInstanceState(outState);
+	        wv.saveState(outState);
+	        av.saveState(outState);
+	    }
+
+	    @Override
+	    public void onActivityCreated(Bundle savedInstanceState) {
+	        super.onActivityCreated(savedInstanceState);
+	        wv.restoreState(savedInstanceState);
+	        av.restoreState(savedInstanceState);
+	    }
+	    
+	    public void onPause() {
+	        super.onPause();
+	        webViewBundle = new Bundle();
+	        wv.saveState(webViewBundle);
+	        av.saveState(webViewBundle);
+	    }
+	    
+	    @Override
+	    public void onViewCreated(View view, Bundle savedInstanceState) {
+	        super.onViewCreated(view, savedInstanceState);
+	        WebView adview = (WebView) view.findViewById(R.id.adview);
+	        new ReloadWebView(act, 60, adview);//refresh ads after 60 seconds
+	    }
+	}
+
+	@Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+		// Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+    
+    protected static class ReloadWebView extends TimerTask { //does what it says
+        Activity context;
+        Timer timer;
+        WebView wv;
+
+        public ReloadWebView(Activity context, int seconds, WebView wv) {
+            this.context = context;
+            this.wv = wv;
+
+            timer = new Timer();
+            timer.schedule(this,
+                    seconds * 1000,  // initial delay
+                    seconds * 1000); // subsequent rate
+        }
+
+        @Override
+        public void run() {
+            if(context == null || context.isFinishing()) { //|| wv == null) {
+                // Activity killed
+                this.cancel();
+                return;
+            }
+
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    wv.reload();
+                }
+            });
+        }
+    }
+}
